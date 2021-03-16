fcb061e1f1c9da41ab03c2c5d9b16e855351e565
Karlo <e14912km@student.manchester.ac.uk>
2021-03-16 19:40:31 +0100

    Morgan logging & database access

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require('morgan');
+const connectDB = require("./config/db");
 
 // Load config
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" });
+
+// Connect database
+connectDB();
 
 const app = express();
 
+// Morgan Logging
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan('dev'));
+}
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server is in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server is in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
