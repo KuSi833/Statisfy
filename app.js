5c3ac07e558b73fe6fe8065bc5f35ac98865c4dc
Karlo <e14912km@student.manchester.ac.uk>
2021-03-16 21:28:59 +0100

    handlebars and static folder

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require("./config/db");
 
 // Load config
@@ -15,6 +17,17 @@ const app = express();
 if (process.env.NODE_ENV == "development") {
   app.use(morgan('dev'));
 }
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(
