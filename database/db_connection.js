fe6006b3ce69b8680b5089c5e745fe2597940a25
Ninjadi12 <a_unnithan@hotmail.com>
2021-03-22 22:30:07 +0000

    Changed Some Widget Orientations

--- database/db_connection.js
+++ database/db_connection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "dbhost.cs.man.ac.uk",
-  user: "USER",
-  password: "PASSWORD",
+  user: "h67867",
+  password: "mtlz0678",
   database: "2020_comp10120_w4"
 });
 
