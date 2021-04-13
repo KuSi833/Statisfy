05f64faa0f2c66cdeccf38e3fae5ec42442cb248
Alexandra Mardale <alexandraimardale@gmail.com>
2021-04-13 17:05:26 +0300

    Delete function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
+const User = require('../models/User');
+const TopArtistsShort = require('../models/TopArtistsShort');
+const TopArtistsMedium = require('../models/TopArtistsMedium');
+const TopArtistsLong = require('../models/TopArtistsLong');
+const TopTracksShort = require('../models/TopTracksShort');
+const TopTracksMedium = require('../models/TopTracksMedium');
+const TopTracksLong = require('../models/TopTracksLong');
+
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,12 +20,20 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc    About us page
 // @route   GET /
-router.get('/aboutus', (req, res) => {
-    res.render('aboutus', {
-        layout: 'main',
-        aboutus: true,
-        title: 'About us',
+router.get('/delete', (req, res) => {
+    User.findOneAndRemove({spotifyId: req.user.spotifyId}, (error, dic) => {});
+    TopArtistsShort.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+    TopArtistsMedium.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+    TopArtistsLong.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+    TopTracksShort.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+    TopTracksMedium.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+    TopTracksLong.deleteMany({userId: req.user.spotifyId}, (error, dic) => {});
+
+    res.render('login', {
+      layout: 'login',
     });
 });
+
+
 
 module.exports = router;
